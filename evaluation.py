1c04d6e68733a428888190ed5a50c4782dc9cc3c
leon <s5499974@stud.uni-frankfurt.de>
2025-09-05 13:10:42 +0200

    Added habituation plot after evaluation for the new habituation dict

--- evaluation.py
+++ evaluation.py
@@ -14,7 +14,7 @@ import babybench.utils as bb_utils
 import babybench.eval as bb_eval
 from learn_selftouch.intrinsic_selftouch_count import Wrapper
 
-from learn_selftouch.intrinsic_selftouch_count import Wrapper
+import matplotlib.pyplot as plt
 
 def main():
     
@@ -57,7 +57,19 @@ def main():
         obs, _ = env.reset()
         evaluation.reset()
 
+        # Store habituation values per body-part. For this, simply use the average habituation
+        # of all habituation values of sensors for that body part.
+        habituations = []
+
         for t_idx in range(args.duration):
+
+            # Store habituation every 20 steps.
+            if t_idx % 20 == 0:
+                habituation = env.habituations.copy()
+                for key in habituation.keys():
+                    habituation[key] = np.mean(habituation[key])
+
+                habituations.append(habituation)
 
             # Select action
             #action = env.action_space.sample()
@@ -76,8 +88,32 @@ def main():
 
             # Perform evaluations of step
             evaluation.eval_step(info)
-            
+
         evaluation.end(episode=ep_idx)
+
+        # Save plot of habituation values per body-part.
+        fig, axes = plt.subplots(nrows=5, ncols=5, figsize=(12, 12))
+        body_names = env.touch_params['scales']
+        timesteps = np.array(range(len(habituations))) * 20
+
+        for i in range(5):
+            for j in range(5):
+                idx=5*i+j
+                if idx >= len(body_names) return
+
+                body_name = body_names[idx]
+                body_id = env.model.body(body_name).id
+
+                ax = axes[i, j]
+                habs = []
+                for i in habituations:
+                    habs.append(i[body_id])
+                ax.plot(timesteps, habs, color='green')
+                ax.set_title(body_name)
+
+        plt.tight_layout()
+        plt.savefig('habplot.png')
+
 
 if __name__ == '__main__':
     main()
