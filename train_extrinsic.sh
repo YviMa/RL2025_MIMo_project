7b2b10a6e72bc926dabbd8ed442be8a9a6b2cc20
Lukas Henssler <s8192908@achilles.rbi.informatik.uni-frankfurt.de>
2025-09-06 17:36:02 +0200

    extrinsic script bugfix

--- train_extrinsic.sh
+++ train_extrinsic.sh
@@ -1,5 +1,5 @@
-python learn_selftouch/train_extrinsic.py --train_for 1000000 --name 1
-python learn_selftouch/train_extrinsic.py --train_for 1000000 --name 2
-python learn_selftouch/train_extrinsic.py --train_for 1000000 --name 3
-python learn_selftouch/train_extrinsic.py --train_for 1000000 --name 4
-python learn_selftouch/train_extrinsic.py --train_for 1000000 --name 5
+python learn_selftouch/extrinsic_selftouch_count.py --train_for 1000000 --name 1
+python learn_selftouch/extrinsic_selftouch_count.py --train_for 1000000 --name 2
+python learn_selftouch/extrinsic_selftouch_count.py --train_for 1000000 --name 3
+python learn_selftouch/extrinsic_selftouch_count.py --train_for 1000000 --name 4
+python learn_selftouch/extrinsic_selftouch_count.py --train_for 1000000 --name 5
